34b9bc938616600a949bb92a90425cd6a4a1ae7d
yasir khan <yasirkhanone22@gmail.com>
2024-01-25 18:01:07 +0500

    correct extension .tsx used.

---
+++ src/config.tsx
@@ -0,0 +1,2 @@
+export const API_KEY: string = "75fc3910-569f-4b1c-ad3e-81120cac7169";
+export const API_URL: string = `https://holidayapi.com/v1/holidays?pretty&country=PK&year=2023&key=${API_KEY}` 
